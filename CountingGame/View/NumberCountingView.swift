de68b9658d5b978e5ed7954831710e766a1a8c72
Sujeet Kumar Singh <sujeet.singh2@conduent.com>
2019-10-29 17:44:57 +0530

    imageview change

--- CountingGame/View/NumberCountingView.swift
+++ CountingGame/View/NumberCountingView.swift
@@ -15,41 +15,44 @@ protocol NumberViewProtocol : class {
 class NumberCountingView: UIViewController , NumberViewProtocol {
 
     var intractor : NumberIntractorProtocol?
+    @IBOutlet weak var lblNumberText: UILabel!
     @IBOutlet weak var lblDigit: UILabel!
-    @IBOutlet weak var imgDigit: UIImageView!
     @IBOutlet weak var imgDescription: UIImageView!
     @IBOutlet weak var btnPrev: UIButton!
     @IBOutlet weak var btnNext: UIButton!
     @IBOutlet weak var btnSound: UIButton!
     
-    var counter = 0
+    var counter : Int = 0 {
+        didSet
+        {
+            counter > 1 ? (btnPrev.isUserInteractionEnabled = true) : (btnPrev.isUserInteractionEnabled = false)
+            counter < 10 ? (btnNext.isUserInteractionEnabled = true) : (btnNext.isUserInteractionEnabled = false)
+        }
+    }
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        counter = counter + 1
+        intractor?.prepareData(forNumber: counter)
+    }
+    
     @IBAction func btnPrevClicked (_ sender : Any)
     {
-        if counter >= 2
-        {
-            counter = counter - 1
-            intractor?.prepareData(forNumber: counter)
-        }
-//        counter == 1 ? btnPrev.isUserInteractionEnabled = false : btnPrev.isUserInteractionEnabled = true
+        counter = counter - 1
+        intractor?.prepareData(forNumber: counter)
     }
     
     @IBAction func btnNextClicked (_ sender : Any)
     {
-        if counter <= 9{
         counter = counter + 1
         intractor?.prepareData(forNumber: counter)
-        }
-        else
-        {
-            
-        }
     }
     
     @IBAction func btnSoundClicked (_ sender : Any)
@@ -59,9 +62,23 @@ class NumberCountingView: UIViewController , NumberViewProtocol {
     
     func changePageUi(withData data: CountingData) {
         print("view")
-        lblDigit.text = data.text
-        imgDigit.animate(toView: imgDigit, imageName: data.numberImage, direction: .bottom)
-        imgDescription.animate(toView: imgDescription, imageName: data.numberImage, direction: .bottom)
+//        lblNumberText.text = data.text
+//        lblDigit.animate(toView: imgDigit, imageName: data.numberImage, direction: .bottom)
+        animateNumber(withData: data)
+        imgDescription.animate(toView: imgDescription, imageName: data.descImage, direction: .bottom)
+    }
+    
+    func animateNumber(withData data: CountingData) {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.lblDigit.alpha = 0.0
+            self.lblNumberText.alpha = 0.0
+        }, completion: { [weak self] finished in
+            self?.lblDigit.text = String((self?.counter) ?? 0)
+             self?.lblNumberText.text = data.text
+            UIView.animate(withDuration: 0.8, animations: {
+                self?.lblDigit.alpha = 1.0
+                self?.lblNumberText.alpha = 1.0
+            })})
     }
 
     /*
